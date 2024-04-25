1dff494ab169008d11b6f462878d17efea947b3f
Logon Aluno <logonrmlocal@fiap.com.br>
2024-04-25 20:52:53 -0300

    Inserindo JS para controlar insersão de um foreach no local storage

---
+++ js/cadastrar.js
@@ -0,0 +1,25 @@
+document
+    .querySelector("#botao-cadastrar")
+    .addEventListener("click", (event) => {
+        event.preventDefault()
+
+        const form = document.querySelector("form")
+
+        const tarefa = {
+            titulo: form.titulo.value,
+            descricao: form.descricao.value,
+            pontos: form.pontos.value
+        }
+
+        console.log(form.titulo.value)
+
+        salvar(tarefa)
+    })
+
+function salvar(tarefa){
+    const tarefas = JSON.parse(localStorage.getItem("tarefas")) || []
+    tarefas.push(tarefa)
+    localStorage.setItem("tarefas", JSON.stringify(tarefas))
+
+    window.location = "index.html"
+}
